28feabc6b50ff1864e4f4731484a8914fc3c978b
Shannon Ervin <sjervin07@gmail.com>
2020-04-07 01:14:18 -0500

    Updates

---
+++ precache-manifest.3c7ff9fc32157090ddedbc41cabb474c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01e966752712890bfbce6e468ec455c2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "be7afebab8782748920f",
+    "url": "/portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "8c743c916a0e4655b497",
+    "url": "/portfolio/static/css/main.f1889de2.chunk.css"
+  },
+  {
+    "revision": "be7afebab8782748920f",
+    "url": "/portfolio/static/js/2.d68a4071.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portfolio/static/js/2.d68a4071.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c743c916a0e4655b497",
+    "url": "/portfolio/static/js/main.d45ec7d9.chunk.js"
+  },
+  {
+    "revision": "1e74db4740f99c68a0bf",
+    "url": "/portfolio/static/js/runtime-main.b37861ed.js"
+  }
+]);
